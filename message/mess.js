77d8c34f8c191aa1a41e175e30eaad77bf331d8a
Kenn <83052195+Denny123141@users.noreply.github.com>
2022-01-09 10:04:26 +0700

    Update mess.js

--- message/mess.js
+++ message/mess.js
@@ -1,17 +1,17 @@
 module.exports.mess = {
-	        wait: '*Wait Now*',
-			success: '*Succes*',
-			wrongFormat: '*Format Is Wrong Retry..!*',
+	        wait: '*Tunggu*',
+			success: '*Sukses*',
+			wrongFormat: '*salah silahkan coba lagi..!*',
 			error: {
-				api: '*Oops An Error Occurred*',
-				stick: '*Not That Sticker :v*',
-				Iv: '*Invalid Link*'
+				api: '*Api eror*',
+				stick: '*Bukan Sticker :v*',
+				Iv: '*Link salah*'
 			},
 			only: {
-				group: '*This command is specific to within the Group only.*',
-				admin: '*This command is specific to within the Group only.*',
-				premium: '*You are not a premium user, send an order* *!buy premium* *to buy premium*',
-				owner: '*This command is specific to the Owner Bot only.*',
-				Badmin: '*This command is only for bots when you are an admin!!*',
+				group: '*Cuma bisa di grup.*',
+				admin: '*Cuma admin grup yang bisa.*',
+				premium: '*Cuma premium yang bisa*',
+				owner: '*Cuma owner yang bisa.*',
+				Badmin: '*Jadin Bot admin dulu!!*',
 			}
 		}
